c91ea22b32f449814251bfa657648ab72ea61095
k2archer <k2archer@hotmail.com>
2019-03-11 21:30:47 +0800

    修正插入数据时，没有完整提交到数据库

--- build/service/v.1.0/user/DatabaseOpenHelper.py
+++ build/service/v.1.0/user/DatabaseOpenHelper.py
@@ -1,9 +1,12 @@
-# -*- coding: utf-8 -*
+﻿# -*- coding: utf-8 -*
 
 import MySQLdb
 import Config
 from Config import DatabaseInfo
 from Config import TableConfig
+from warnings import filterwarnings
+
+filterwarnings('error', category=MySQLdb.Warning)
 
 
 class DatabaseOpenHelper:
@@ -32,8 +35,8 @@ class DatabaseOpenHelper:
         else:
             # print 'db is connected.'
             self.db_cursor = self.db_connection.cursor()
-            self.db_cursor.execute(Config.DatabaseConfig.deleteDatabase)
-            self.db_cursor.execute(Config.DatabaseConfig.createDatabase)
+            self._execute(Config.DatabaseConfig.deleteDatabase)
+            self._execute(Config.DatabaseConfig.createDatabase)
             # data = self.db_cursor.fetchone()
             # print data
             return self.db_cursor
@@ -43,15 +46,23 @@ class DatabaseOpenHelper:
         if self.db_cursor is None:
             print 'db cursor is None'
             return
-        else:
-            self.db_cursor.execute("USE " + Config.DatabaseConfig.DATABASE_NAME)
-            self.db_cursor.execute(TableConfig.createUserInfoTable)
-            self.db_cursor.execute(TableConfig.createBookTable)
-            self.db_cursor.execute(TableConfig.createOrderListTable)
-            pass
+
+        self._execute("USE " + Config.DatabaseConfig.DATABASE_NAME)
+        self._execute(TableConfig.createUserInfoTable)
+        self._execute(TableConfig.createBookTable)
+        self._execute(TableConfig.createOrderListTable)
+
         pass
 
-    pass
+    def _execute(self, query):
+        try:
+            self.db_cursor.close()
+            self.db_cursor = self.db_connection.cursor()
+            self.db_cursor.execute(query)
+        except MySQLdb.Warning, w:
+            print str(w)
+
+        pass
 
     def demo_data(self):
         if self.db_cursor is None:
@@ -69,7 +80,7 @@ class DatabaseOpenHelper:
             # data = self.db_cursor.fetchall()
             # print data
 
-            demo_book = [['9787121269394', 'Android 1'], ['9787115439789', 'Android 2']]
+            demo_book = [['9787121269394', u'Android 开发艺术探索'], ['9787115439789', u'Android 第一行代码']]
             for i in demo_book:
                 add_test_book_info = "INSERT INTO " + TableConfig.BookInfo.TABLE_NAME \
                                      + " ( " + TableConfig.BookInfo.CODE + ', ' + TableConfig.BookInfo.NAME + " )" \
@@ -80,17 +91,19 @@ class DatabaseOpenHelper:
             # data = self.db_cursor.fetchall()
             # print data
 
-            demo_order = [['1', '2'], ['2', '2']]
+            demo_order = [['1', '1'], ['1', '2'], ['2', '2']]
             for i in demo_order:
                 add_test_orders_list = "INSERT INTO " + TableConfig.Order.TABLE_NAME \
-                                     + " ( " + TableConfig.UserInfo.ID + ', ' + TableConfig.BookInfo.ID + " )" \
-                                     + " VALUES (" + '\'' + i[0] + '\'' + ',' + '\'' + i[1] + '\'' + ')'
+                                       + " ( " + TableConfig.UserInfo.ID + ', ' + TableConfig.BookInfo.ID + " )" \
+                                       + " VALUES (" + '\'' + i[0] + '\'' + ',' + '\'' + i[1] + '\'' + ')'
                 self.db_cursor.execute(add_test_orders_list)
 
             # self.db_cursor.execute("select * from " + TableConfig.Order.TABLE_NAME)
             # data = self.db_cursor.fetchall()
             # print data
 
+        self.db_connection.commit()
+        # self.db_connection.close()
         pass
 
 
@@ -98,6 +111,7 @@ db_helper = DatabaseOpenHelper()
 
 
 def get_db_helper():
+    # type: () -> DatabaseOpenHelper
     return db_helper
 
 
